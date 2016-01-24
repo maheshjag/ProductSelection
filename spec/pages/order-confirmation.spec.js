50a7ea557a2a73ec2de141467ebc74663dc88b2a
Mahesh Jagadeesan <maheshj@gmx.net>
2016-01-24 00:39:11 +0000

    Missed adding confirmation page spec to previous commit

---
+++ spec/pages/order-confirmation.spec.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var OrderConfirmation = require('../../src/pages/order-confirmation');
+
+describe('Order confirmation page', function () {
+	it('should return false if input contains only location', function () {
+		var result = OrderConfirmation.validateInputs({location: 'test1'});
+		expect(result).toBeDefined();
+		expect(result).toBe(false);
+	});
+	it('should return false if input contains only products', function () {
+		var result = OrderConfirmation.validateInputs({product: ['test1']});
+		expect(result).toBeDefined();
+		expect(result).toBe(false);
+	});
+	it('should return true only if input contains both location and products', function () {
+		var result = OrderConfirmation.validateInputs({location: 'test1', product: []});
+		expect(result).toBeDefined();
+		expect(result).toBe(true);
+	});
+});
